ae594ae521ed5cfc86390ed65823d9d4c3a98b04
Temateu Roslyn <temateuroslynf32@gmail.com>
2023-02-26 16:31:30 +0100

    assemblage des packae dans le projet

---
+++ package-scala.sh
@@ -0,0 +1,8 @@
+cd src/backend/scala
+
+# [1] Faire une installation des dependances maven + package
+mvn clean package
+
+# [2] executer le jar file
+
+java -jar target/big-data-training-scala-kafka-1.0-SNAPSHOT.jar
